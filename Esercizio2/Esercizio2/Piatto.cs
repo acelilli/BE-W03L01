9ade6d3ea805e7541f2684b72f6908f5b1013c48
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-02-12 16:55:30 +0100

    Up esercizio 2

---
+++ Esercizio2/Esercizio2/Piatto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio2
+{
+    internal class Piatto
+    {
+        //Tutte le proprietà del piatto
+        public int NumMenu { get; set; }
+        public string Nome { get; set; }
+        public decimal Prezzo { get; set; }
+
+        // COSTRUTTORE
+        public Piatto(int numMenu, string nome, decimal prezzo)
+        {
+            NumMenu = numMenu;
+            Nome = nome;
+            Prezzo = prezzo;
+        }
+
+        // Creazione del menù array utilizzando il costruttore
+        public static Piatto[] Menu { get; } = new Piatto[]
+    {
+        new Piatto(1, "Coca Cola 150 ml", 2.50m),
+        new Piatto(2, "Insalata di pollo", 5.20m),
+        new Piatto(3, "Pizza Margherita", 10.00m),
+        new Piatto(4, "Pizza 4 Formaggi", 12.50m),
+        new Piatto(5, "Pz patatine fritte", 3.50m),
+        new Piatto(6, "Insalata di riso", 8.00m),
+        new Piatto(7, "Frutta di stagione", 5.00m),
+        new Piatto(8, "Pizza fritta", 5.00m),
+        new Piatto(9, "Piadina vegetariana", 6.00m),
+        new Piatto(10, "Panino Hamburger", 7.90m)
+    };
+
+        // METODI:
+        //1. 
+    }
+}
---
+++ Esercizio2/Esercizio2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int scelta;
+
+            // do
+            // {
+                // MENU 
+                Console.WriteLine("==============MENU==============");
+                Console.WriteLine("1: Coca Cola 150 ml (€ 2.50)");
+                Console.WriteLine("2: Insalata di pollo (€ 5.20)");
+                Console.WriteLine("3: Pizza Margherita (€ 10.00)");
+                Console.WriteLine("4: Pizza 4 Formaggi (€ 12.50)");
+                Console.WriteLine("5: Pz patatine fritte (€ 3.50)");
+                Console.WriteLine("6: Insalata di riso (€ 8.00)");
+                Console.WriteLine("7: Frutta di stagione (€ 5.00)");
+                Console.WriteLine("8: Pizza fritta (€ 5.00)");
+                Console.WriteLine("9: Piadina vegetariana (€ 6.00)");
+                Console.WriteLine("10: Panino Hamburger (€ 7.90)");
+                Console.WriteLine("11: Stampa conto finale e conferma");
+                Console.WriteLine("========================================");
+                // FINE MENU
+                // Legge l'input dell'utente
+               // string input = Console.ReadLine(); // valore dell'input
+                
+              //  if (int.TryParse(input, out scelta)) // parse di input -> scelta
+               /* {
+                    switch (scelta)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+
+                            break;
+                        case 11:
+
+                        default:
+                            Console.WriteLine("Scelta non valida.");
+                            break;
+                    }
+                   } else
+                    {
+                        Console.WriteLine("Input non valido. Inserisci un numero!");
+                    }
+                } while (" "); // Continua fino a stop
+        }
+    }
+}
